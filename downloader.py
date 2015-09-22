2e89770d63c2360908e6b8136f3db5ae18785347
U-WHQ_NT_DOMAIN\MH024332 <MH024332@CERN-MH024332.northamerica.cerner.net>
2015-09-22 14:47:48 -0500

    seperated code to multiple methods. added logging and exception handling

--- downloader.py
+++ downloader.py
@@ -2,22 +2,48 @@ import time
 import schedule
 import csv
 import subprocess
+import logging
+from datetime import datetime
+
+def loadConfig():
+	with open('channels.csv', 'rb') as csvFile
+		read = csv.reader(csvFile)
+		headerLine = True
+		rowsList = []
+		for row in reader:
+			if headerLine
+				headerLine = False
+				continue
+			rowTup = (row[0], row[1])
+			rowsList.append(rowTup)
+		return rowsList
+
+def buildArtistName(artistName):
+	artistName = artistName.replace(' ', '\ ')
+	return artistName
+
+def buildOutputPath(artistName):
+	outputpath = 'music/' + artistname + '\ -\ ' + 'Uploads' + '/%(title)s.%(ext)s'
+	outputpath = outputpath.replace('(', '\(')
+	outputpath = outputpath.replace(')', '\)')
+	return outputpath
 
 def download():
-	with open('channels.csv','rb') as csvFile:
-		reader = csv.reader(csvFile)
-		firstline = True
-		for row in reader:
-			if firstline:
-				firstline = False
-				continue
-			artistname = row[1]
-			artistname = artistname.replace(' ', '\ ')
-			outputpath = 'music/' + artistname + '\ -\ ' + 'Uploads' + '/%(title)s.%(ext)s'
-			outputpath = outputpath.replace('(', '\(')
-			outputpath = outputpath.replace(')', '\)')
-			subprocess.check_call('youtube-dl --add-metadata --embed-thumbnail --playlist-end 10 --dateafter now-1month -f 140 -i -o ' + outputpath + ' ' + row[0], shell=True)
+	logging.info('Begin Processing: ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+	config = loadConfig()
+	for row in config:
+		artistname = buildArtistName(row[1])
+		outputpath = buildOutputPath(artistname)
+		script = 'youtube-dl --add-metadata --embed-thumbnail --playlist-end 10 --dateafter now-1month -f 140 -i -o ' + outputpath + ' ' + row[0]
+		with open('youtube-dl-log.log', "w") as outfile:
+			try:
+				subprocess.check_call(script, shell=True, stdout=outfile)
+			except:
+				logging.info('Something went wrong while executing command.')
+	logging.info('End Processing: ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
+
+logging.basicConfig(filename='app.log', level=logging.INFO)
 download()
 schedule.every(30).minutes.do(download)
 
